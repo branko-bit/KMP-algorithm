043b18c2635d502976b86610461558e0af9ed5c9
branko-bit <brankomajsen@gmail.com>
2025-04-08 15:11:08 +0200

    changed sh for testing

--- tests/test_script.sh
+++ tests/test_script.sh
@@ -13,16 +13,32 @@ fi
 echo "Izvajanje testov..."
 
 # Test 1: KMP algoritem
-./program 0 "niz" "primer_besedilo.txt"
+./program 0 "Lorem ipsum" "primer_besedilo.txt"
 if [ $? -ne 0 ]; then
   echo "Test 1 (KMP) ni uspel!"
+  cat out.txt
+  exit 1
+fi
+
+# Primerjaj izhod z pravilnim izhodom
+diff out.txt primer_izhoda.txt
+if [ $? -ne 0 ]; then
+  echo "Test 1 (KMP) ni uspel! Izhod se ne ujema s pravilnim izhodom."
+  cat out.txt
   exit 1
 fi
 
 # Test 2: Sunday algoritem
-./program 1 "niz" "primer_besedilo.txt"
+./program 1 "Lorem ipsum" "primer_besedilo.txt"
 if [ $? -ne 0 ]; then
   echo "Test 2 (Sunday) ni uspel!"
+  exit 1
+fi
+
+# Primerjaj izhod z pravilnim izhodom
+diff out.txt primer_izhoda.txt
+if [ $? -ne 0 ]; then
+  echo "Test 2 (Sunday) ni uspel! Izhod se ne ujema s pravilnim izhodom."
   exit 1
 fi
 
